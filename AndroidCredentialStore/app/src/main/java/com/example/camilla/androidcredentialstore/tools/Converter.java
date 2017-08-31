14591f8b49d9a4214752fd076c33e924a21c9859
milld10 <c.reis@student.tugraz.at>
2017-08-31 18:27:31 +0200

    everything done and working, now moving on to bluetooth API

--- AndroidCredentialStore/app/src/main/java/com/example/camilla/androidcredentialstore/tools/Converter.java
+++ AndroidCredentialStore/app/src/main/java/com/example/camilla/androidcredentialstore/tools/Converter.java
@@ -1,35 +1,16 @@
 package com.example.camilla.androidcredentialstore.tools;
 
 import android.support.design.widget.TextInputEditText;
-import android.util.Log;
-import android.widget.EditText;
-
 import com.example.camilla.androidcredentialstore.models.Account;
-
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 
-/**
- * Created by Camilla on 12.08.2017.
- */
 
 public class Converter {
 
     /*
     private static final char[] HEX_DIGITS = "0123456789abcdef".toCharArray();
-
-    private static String byteArrayToString(byte[] in)
-    {
-        char out[] = new char[in.length * 2];
-        for (int i = 0; i < in.length; i++)
-        {
-            out[i * 2] = "0123456789ABCDEF".charAt((in[i] >> 4) & 15);
-            out[i * 2 + 1] = "0123456789ABCDEF".charAt(in[i] & 15);
-        }
-        return new String(out);
-    }
-
 
     private static char[] toHex(byte[] bytes)
     {
@@ -56,9 +37,7 @@ public class Converter {
         pw.getText().getChars(0, length, charArray, 0);
 
         ByteBuffer buffer = StandardCharsets.UTF_8.encode(CharBuffer.wrap(charArray));
-
         byte[] byteArray = new byte[buffer.limit()];
-
         buffer.get(byteArray);
 
         return byteArray;
@@ -68,8 +47,8 @@ public class Converter {
     public static char[] byteToChar(Account account)
     {
         byte[] byteArray = account.getPassword();
-        CharBuffer buffer = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(byteArray));
 
+        CharBuffer buffer = StandardCharsets.UTF_8.decode(ByteBuffer.wrap(byteArray));
         char[] charArray = new char[buffer.limit()];
         buffer.get(charArray);
 
