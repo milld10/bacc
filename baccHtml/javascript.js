8b81e5243ef634151f6fb6a5d059b1787bdb4568
Camilla Reis <c.reis@student.tugraz.at>
2018-10-03 13:18:26 +0200

    misc files wandering around, now pushed

---
+++ baccHtml/javascript.js
@@ -0,0 +1,54 @@
+
+const customServiceUUID = "0000a000-0000-1000-8000-00805f9b34fb";
+const characteristicUsernameUUID = "0000a001-0000-1000-8000-00805f9b34fb";
+const characteristicPasswordUUID = "0000a002-0000-1000-8000-00805f9b34fb";
+
+    function onButtonClick() {
+        navigator.bluetooth.requestDevice({
+                filters: [{
+                  services: [customServiceUUID]
+                }]
+            })
+            .then(device => {
+                console.log("Device name: " + device.name);
+                return device.gatt.connect();
+            })
+            .then(server => {
+                console.log("getting primary service");
+                return server.getPrimaryService(customServiceUUID);
+            })
+            .then(service => {
+                console.log("getting username characteristic");
+                
+                return service.getCharacteristics();
+            })
+            .then(characteristics => {
+                console.log("reading value of characteristic");
+                console.log('> Characteristics: ' + characteristics.map(c => c.uuid).join('\n' + ' '.repeat(19)));
+                
+                console.log('> Characteristics: ' + characteristics.map(c => c.readValue()
+                  .then(function(dataView)
+                  {
+                    var enc = new TextDecoder("utf-8");
+                    var help = enc.decode(dataView.buffer);
+                    if(c.uuid == characteristicUsernameUUID){
+                      document.getElementById("username").value = help;
+                    }
+                    if(c.uuid == characteristicPasswordUUID) {
+                      document.getElementById("password").value = help;
+                    }
+                  
+                  })));
+                
+                console.log("Now done.");
+            })
+            .catch(error => { console.log("Error Message: " + error); });
+    }
+   
+ 
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        onButtonClick();
+
+    });
